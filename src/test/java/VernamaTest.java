87a897f97469378c623a572f7148c1ceb122f025
Paweł Żydek <pablo977spox@interia.eu>
2017-05-04 21:43:18 +0200

    v_4

--- src/test/java/VernamaTest.java
+++ src/test/java/VernamaTest.java
@@ -1,9 +1,8 @@
-import com.google.common.collect.Lists;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 import java.util.Arrays;
-import java.util.List;
 
 
 public class VernamaTest {
@@ -18,7 +17,7 @@ public class VernamaTest {
 
 
     @Test
-    public void shouldLoadDataFromFile(){
+    public void shouldLoadDataFromFile() {
         //when
         String result = vernama.loadDataFromFile();
 
@@ -27,7 +26,7 @@ public class VernamaTest {
     }
 
     @Test
-    public void shouldReturnEmptyResultWhenFileIsInvalid(){
+    public void shouldReturnEmptyResultWhenFileIsInvalid() {
         //given
         Vernama vernama = new Vernama("notExistingFile.txt");
 
@@ -37,20 +36,18 @@ public class VernamaTest {
         //then
         Assert.assertEquals("", result);
 
-        List<Integer> jjj = Lists.newArrayList(1,2,3,4,5);
-
     }
 
     @Test(expected = NullPointerException.class)
-    public void shouldThrowNPE(){
+    public void shouldThrowNPE() {
         //when
         vernama.changeToBinary(null);
     }
 
     @Test
-    public void shouldTestChangeToBinary(){
+    public void shouldTestChangeToBinary() {
         //given
-        byte[] expected = { (byte) 75, (byte) 79, (byte) 84 };
+        byte[] expected = {(byte) 75, (byte) 79, (byte) 84};
 
         //when
         byte[] result = vernama.changeToBinary("KOT");
@@ -61,24 +58,27 @@ public class VernamaTest {
     }
 
     @Test
-    public void shouldTestBinaryChangeToText(){
+    public void shouldTestBinaryChangeToText() {
         //given
         String expected = "ko";
+
         //when
-       String result = vernama.binaryToText(new int[] {0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1});
+        String result = vernama.binaryToText(new int[]{0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1});
 
         //then
-        Assert.assertEquals(expected,result);
+        Assert.assertEquals(expected, result);
 
 
     }
 
     @Test
-    public void shouldTestChangeDecimalToBinary(){
+    public void shouldTestChangeDecimalToBinary() {
         //given
-        int[] expected = {0,1,0,1,0,1,0,0};
+        int[] expected = {0, 1, 0, 1, 0, 1, 0, 0};
+
         //when
         int[] result = vernama.decimalToBinary(new byte[]{84});
+
         //then
         Assert.assertEquals(Arrays.toString(expected), Arrays.toString(result));
 
@@ -86,12 +86,12 @@ public class VernamaTest {
 
 
     @Test
-    public void shouldDecryptWhenPassedValueIsOK(){
+    public void shouldDecryptWhenPassedValueIsOK() {
         //given
         String expected = "ko";
 
         //when
-        String result = vernama.decrypt(new int[] {0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1});
+        String result = vernama.decrypt(new int[]{0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1});
 
         //then
         Assert.assertEquals(expected, result);
@@ -99,11 +99,13 @@ public class VernamaTest {
     }
 
     @Test
-    public void shouldDoDecodedValue(){
+    public void shouldDoDecodedValue() {
         //given
-        int[] expected = {1, 1, 0 ,0};
+        int[] expected = {1, 1, 0, 0};
+
         //when
-       int[] result=  vernama.doDecoded(new int[]{1, 0, 0, 1}, new int[] {0, 1, 0, 1});
+        int[] result = vernama.doDecoded(new int[]{1, 0, 0, 1}, new int[]{0, 1, 0, 1});
+
         //then
         Assert.assertEquals(Arrays.toString(expected), Arrays.toString(result));
     }
@@ -112,10 +114,12 @@ public class VernamaTest {
     public void shouldDoEncodedValue() {
         //given
         int[] expected = {0, 1, 1, 1};
+
         //when
         int[] result = vernama.doEncode(new int[]{1, 0, 0, 0}, new int[]{1, 1, 1, 1});
+
         //then
         Assert.assertEquals(Arrays.toString(expected), Arrays.toString(result));
     }
 
-    }
+}
